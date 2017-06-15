39b4800329ccaaf48089884473343357b6e32294
martensonbj <martensonbj@gmail.com>
2017-06-15 11:14:10 -0600

    Rename reducer boilerplate

--- src/reducers/image.js
+++ src/reducers/image.js
@@ -1,12 +1,12 @@
 const initialState = {
-  
+
 }
 
-const backgroundImage = (state = initialState, action) => {
+const image = (state = initialState, action) => {
   switch (action.type) {
     default :
       return state;
   }
 }
 
-export default backgroundImage
+export default image
