eb713604f84b4dc064c8a86a0a1362d2de9bc870
martensonbj <martensonbj@gmail.com>
2017-06-15 17:11:11 -0600

    Redux actions wired up

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import thunk from 'redux-thunk';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers';
-
 import './styles.css';
+
+import { getImage } from './actions';
+
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
-    applyMiddleware()
+    applyMiddleware(thunk)
   ),
 );
 
+store.dispatch(getImage());
 
 render(
   <Provider store={ store }>
